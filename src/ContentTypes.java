9569462bc6ae61c4a2e7a9375d988e55f6038843
Andy Gherna <argherna@gmail.com>
2018-04-14 20:12:18 -0500

    Fix uploads through web UI

--- src/ContentTypes.java
+++ src/ContentTypes.java
@@ -8,11 +8,12 @@ class ContentTypes {
   static {
     Map<String, String> types = new HashMap<>();
     types.put("css", "text/css");
+    types.put("form", "application/x-www-form-urlencoded");
     types.put("html", "text/html");
     types.put("js", "application/javascript");
-    types.put("map", "application/javascript");
-    types.put("form", "application/x-www-form-urlencoded");
     types.put("json", "application/json");
+    types.put("map", "application/javascript");
+    types.put("upload", "multipart/form-data");
     types.put("xml", "text/xml");
     TYPES = Collections.unmodifiableMap(types);
   }
--- src/SettingsHandler.java
+++ src/SettingsHandler.java
@@ -1,13 +1,23 @@
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Logger;
 import java.util.prefs.BackingStoreException;
+import java.util.prefs.InvalidPreferencesFormatException;
 
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 class SettingsHandler implements HttpHandler {
+
+  private static final Logger LOGGER = Logger.getLogger(
+    SettingsHandler.class.getName());
+
+  private static final String BOUNDARY = ContentTypes.TYPES.get("upload") + "; boundary=";
 
   @Override
   public void handle(HttpExchange exchange) throws IOException {
@@ -16,12 +26,15 @@ class SettingsHandler implements HttpHandler {
       case "GET":
         doGet(exchange);
         break;
+      case "POST":
+        doPost(exchange);
+        break;
       default:
         Map<String, List<String>> responseHeaders = new HashMap<>();
         Http.addServerHeaders(responseHeaders, Pike.SERVER_STRING);
         Http.addContentTypeResponseHeaders(responseHeaders, 
           ContentTypes.TYPES.get("json"));
-        responseHeaders.put("Allow", List.of("GET"));
+        responseHeaders.put("Allow", List.of("GET", "POST"));
         byte[] content = Json.renderError(
           String.format("Method %s not allowed!", method)).getBytes();
         Http.sendResponse(exchange, HttpStatus.METHOD_NOT_ALLOWED, content, 
@@ -55,5 +68,41 @@ class SettingsHandler implements HttpHandler {
     Http.addContentTypeResponseHeaders(responseHeaders, 
       contentType);
     Http.sendResponse(exchange, status, content, responseHeaders);
+  }
+
+  private void doPost(HttpExchange exchange) throws IOException {
+    Headers h = exchange.getRequestHeaders();
+    String contentType = h.getFirst("Content-Type");
+    HttpStatus status = HttpStatus.NO_CONTENT;
+    byte[] content = new byte[0];
+    if (contentType.startsWith(ContentTypes.TYPES.get("upload"))) {
+      byte[] uploadedData = getUploadedData(exchange, contentType);
+      try {
+        Settings.importSettings(uploadedData);
+      } catch (InvalidPreferencesFormatException e) {
+        throw new RuntimeException(e);
+      }
+    } else {
+      status = HttpStatus.BAD_REQUEST;
+    }
+    Http.sendResponse(exchange, status, content, 
+      Collections.<String, List<String>>emptyMap());
+  }
+
+  private byte[] getUploadedData(HttpExchange exchange, String contentType) 
+  throws IOException {
+    String boundary = contentType.substring(BOUNDARY.length());
+    String[] requestBodyLines = new String(exchange.getRequestBody().readAllBytes()).split("\\n");
+    ByteArrayOutputStream bos = new ByteArrayOutputStream();
+    for (String line : requestBodyLines) {
+      // Don't process the multipart boundary, any Content lines, or 
+      // processing instructions.
+      if (!line.contains(boundary) && 
+        !line.startsWith("Content") && 
+        !line.isEmpty() && !line.startsWith("<?")) {
+        bos.write(line.getBytes());
+      }
+    }
+    return bos.toByteArray();
   }
 }
